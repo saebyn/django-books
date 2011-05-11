8bc02a8f61ee0031b81f5a539c1ec2bbef51d6a1
John Weaver <john@pledge4code.com>
2011-05-11 15:51:48 -0700

    Renamed package for setup purposes.

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
 from setuptools import setup
 
-setup(name='django_books',
-      version='0.0.1',
-      author='John Weaver',
-      author_email='john@pledge4code.com',
-      description='A reusable Django app for managing books.',
-      url='http://github.com/saebyn/django-books/',
-      package_dir={'': 'books'},
-      install_requires=[],
-      classifiers=[
+setup(name = 'django-books',
+      version = '0.0.1',
+      author = 'John Weaver',
+      author_email = 'john@pledge4code.com',
+      description = 'A Django reusable app for managing books.',
+      long_description = open('README.rst').read(),
+      url = 'http://github.com/saebyn/django-books/',
+      packages = ['django_books'],
+      license = 'BSD',
+      classifiers = [
         'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: New BSD License',
-        'Programming Language :: Python :: 2.6',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
       ],
-      platforms=('Any',),
-     )
+)
